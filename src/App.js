cd5ecc8d07f32a5f8ed00c98b72fbdc1123757d9
ritikts <webofritik@gmail.com>
2022-03-25 07:35:45 +0530

    Expense

--- src/App.js
+++ src/App.js
@@ -1,50 +1,31 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   let expense = [
     {
       title: "School Fee",
       amount: 4000,
-      date: new Date(2022, 4, 23)
+      date: new Date(2022, 4, 23),
     },
     {
       title: "Tution Fee",
       amount: 9000,
-      date: new Date(2022, 4, 24)
+      date: new Date(2022, 5, 24),
     },
     {
       title: "College Fee",
       amount: 2000,
-      date: new Date(2022, 4, 25)
+      date: new Date(2022, 6, 25),
     },
     {
       title: "House Rent",
       amount: 40000,
-      date: new Date(2022, 4, 26)
-    }
-  ]
+      date: new Date(2022, 8, 26),
+    },
+  ];
   return (
     <div>
-      <ExpenseItem
-        date={expense[0].date}
-        amount={expense[0].amount}
-        title={expense[0].title}
-      />
-      <ExpenseItem
-        date={expense[1].date}
-        amount={expense[1].amount}
-        title={expense[1].title}
-      />
-      <ExpenseItem
-        date={expense[2].date}
-        amount={expense[2].amount}
-        title={expense[2].title}
-      />
-      <ExpenseItem
-        date={expense[3].date}
-        amount={expense[3].amount}
-        title={expense[3].title}
-      />
+      <Expenses item={expense} />
     </div>
   );
 }
