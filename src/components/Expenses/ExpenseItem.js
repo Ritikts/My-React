a1caef6824f011b3d056563d3f8bfb3344a72d3f
ritikts <webofritik@gmail.com>
2022-03-27 20:44:28 +0530

    react

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const changeTitle = (e) => {
+    setTitle("New");
+  };
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={changeTitle}>Change Title</button>
     </div>
   );
-}
+};
 export default ExpenseItem;
